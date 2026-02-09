5a301391933b5aad755332d7b8da7d39fa1c14d8
Dominik Lehrl <d.lehrl@remind.de>
2026-02-09 12:49:28 +0100

    fix: use default properties if no properties were selected in plugin configs

--- Classes/Event/Listener/SerializeEntityEventListener.php
+++ Classes/Event/Listener/SerializeEntityEventListener.php
@@ -7,11 +7,11 @@ namespace Remind\Contacts\Event\Listener;
 use Remind\Contacts\Domain\Model\Contact;
 use Remind\Contacts\Domain\Model\Group;
 use Remind\Extbase\Event\SerializeEntityEvent;
+use Remind\Headless\Service\FilesService;
 use TYPO3\CMS\Core\Attribute\AsEventListener;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Domain\Model\FileReference;
 use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
-use Remind\Headless\Service\FilesService;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 #[AsEventListener]
 final readonly class SerializeEntityEventListener
@@ -22,17 +22,22 @@ final readonly class SerializeEntityEventListener
     {
         $this->filesService = GeneralUtility::makeInstance(FilesService::class);
     }
+
     public function __invoke(SerializeEntityEvent $event): void
     {
         $abstractEntity = $event->getAbstractEntity();
-        $settings =  $event->getSettings();
+        $settings = $event->getSettings();
+
+        if (($settings['properties'] ?? '') === '') {
+            $settings['properties'] = implode(',', array_keys($abstractEntity->_getProperties()));
+        }
+
         $properties = explode(',', $this->camelize($settings['properties']));
 
         if ($abstractEntity instanceof Contact) {
             /** @var \TYPO3\CMS\Core\TypoScript\FrontendTypoScript $frontendTyposcript */
             $frontendTyposcript = $event->getRequest()->getAttribute('frontend.typoscript');
             $constants = $frontendTyposcript->getFlatSettings();
-            $normalizedParams = $event->getRequest()->getAttribute('normalizedParams');
 
             $pageType = (int) $constants['plugin.tx_contacts.vcard.typeNum'];
 
@@ -45,6 +50,10 @@ final readonly class SerializeEntityEventListener
             $json = $event->getJson();
 
             foreach ($properties as $property) {
+                if ($property === '') {
+                    continue;
+                }
+
                 $value = $abstractEntity->_getProperty($property);
 
                 if ($value instanceof FileReference) {
@@ -55,10 +64,10 @@ final readonly class SerializeEntityEventListener
                     foreach ($value as $item) {
                         if ($item instanceof Group) {
                             $storageItems[] = [
-                                'uid' => $item->getUid(),
-                                'pid' => $item->getPid(),
-                                'name' => $item->getName(),
                                 'description' => $item->getDescription(),
+                                'name' => $item->getName(),
+                                'pid' => $item->getPid(),
+                                'uid' => $item->getUid(),
                             ];
                         }
                     }
@@ -76,7 +85,7 @@ final readonly class SerializeEntityEventListener
         }
     }
 
-    protected function camelize($input, $separator = '_')
+    protected function camelize(string $input, string $separator = '_'): string
     {
         return lcfirst(str_replace($separator, '', ucwords($input, $separator)));
     }
